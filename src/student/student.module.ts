0f3780b7a3e5c34ff6df9d596943c663247d2fd2
ZekDaniels <zekavsrdev@gmail.com>
2022-10-12 06:10:05 +0300

    #8 feat: student model created. create, list, detail got for student

---
+++ src/student/student.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { StudentResolver } from "./Student.resolver";
+import { StudentService } from "./Student.service";
+import { Student } from "./student.entity";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Student])],
+  providers: [StudentResolver, StudentService],
+})
+export class StudentModule {}
